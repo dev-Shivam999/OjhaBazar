99ca665f686f86c4e56fb199667c58cf358c6f3f
dev-Shivam999 <shivamswami982@gmail.com>
2025-02-07 14:30:40 +0530

    feat: refactor cart API to use addToCart function and improve error handling; update userId variable naming

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
 import Items from "./../components/Items";
 
 const Page = async () => {
   return (
     <div className="container mx-auto p-6">
         <Items />
+      <Link href={'/Add'}>  <div className="fixed bottom-1 right-1 text-5xl bg-white p-3 rounded-full w-max text-black">
+        +
+      </div></Link>
     </div>
   );
 };
